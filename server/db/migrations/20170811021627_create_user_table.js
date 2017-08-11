ccf539b4c70bd96c63282de92c94f8d12e1d9c4b
Helen <howareyouhelen@hotmail.com>
2017-08-11 20:28:05 +0000

    created a user table storing user profile info

--- server/db/migrations/20170811021627_create_user_table.js
+++ server/db/migrations/20170811021627_create_user_table.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  
+    return Promise.all([
+        knex.schema.createTable('users', function(table) {
+            table.increments();
+            table.string('username').unique().notNull();
+            table.string('email').unique().notNull();
+            table.string('password').notNull();
+            table.string('tagline');
+            table.string('blurb', 1000);
+            table.string('imageurl');
+            table.smallint('skill').defaultTo(0);
+            table.timestamps();
+        })
+    ])
 };
 
 exports.down = function(knex, Promise) {
-  
+    return Promise.all([
+        knex.schema.dropTable('users')
+    ])
 };
