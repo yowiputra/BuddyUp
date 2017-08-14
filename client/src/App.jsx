0a49c8b69babf92825ee0484f4a7ca08199d83c1
JV Cortez <juanvictor.cortez@gmail.com>
2017-08-14 11:31:11 -0700

    resolved conflicts. added socket. io module and kept navbar format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import Dropdown from './Dropdown.jsx'
-=======
 import io from 'socket.io-client';
 const socket = io.connect('http://127.0.0.1:3001');
->>>>>>> 1853e3e0db41257006a6d7814052d07350bd994e
 
 class App extends Component {
 
@@ -18,13 +14,10 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <nav className="navbar">
           <p className="title">BuddyUp</p>
           <Dropdown />
         </nav>
-=======
->>>>>>> 1853e3e0db41257006a6d7814052d07350bd994e
         <h1>Hello React</h1>
       </div>
     );
