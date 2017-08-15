a0a8ba55b7f7c4024432ab580e0d86cb3011b7dd
John <john.wong.h@gmail.com>
2017-08-15 22:46:35 +0000

    jwt Token saved to localstorage on login

--- client/actions/loginActions.jsx
+++ client/actions/loginActions.jsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 export function login(data) {
   return dispatch => {
-    return axios.post('/api/auth', data)
+    return axios.post('/api/auth', data).then(res => {
+      const token = res.data.token;
+      localStorage.setItem('jwtToken', Token);
+    });
   }
 }
