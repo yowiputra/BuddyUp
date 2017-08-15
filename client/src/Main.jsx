4b3e23c1d5e828db066a116e310825868e613633
JV Cortez <juanvictor.cortez@gmail.com>
2017-08-14 19:22:05 -0700

    added  comments about card design and functions. removed h1.

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -3,17 +3,24 @@ import Cards, { Card } from 'react-swipe-card';
 
 export default class Main extends Component {
   render() {
-    const data = ['Match #1', 'Match #2', 'Match #3'];
+    // data will be an array of objects(most probably JSON), which each objects will contain the information from a user: username, text description, avatar, etc
+    // Each card should have an output for showing username, avatar, text description, and a button to send an invite
+    const data = ['Match #1', 'Match #2', 'Match #3', 'Match #4', 'Match #5', 'Match #6'];
     return (
       <div>
-        <h1>Swipeable Card Stack</h1>
         <Cards onEnd={() => console.log('end')} className='master-root'>
           {data.map(item =>
+          //make card and card border bigger
+          //make sure it's mobile responsive
           <Card
             onSwipeLeft={() => console.log('swipe left')}
             onSwipeRight={() => console.log('swipe right')}>
             <h2>{item}</h2>
-            </Card>
+            <img className="avatar" src="" alt="avatar image"/> 
+            <p>Username</p>
+            <p className="card-text-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae risus risus.</p>
+            <button className="btn-poke">Poke!</button>     
+          </Card>
           )}
         </Cards>
       </div>
