aaf9e586e4704023ed17b3b4d166074a775dc306
John <john.wong.h@gmail.com>
2017-08-17 03:06:45 +0000

    Added route for profile page to get information about the logged in user.

---
+++ client/actions/userDataActions.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import setAuthorizationToken from '../utils/setAuthorizationToken.jsx';
+import jwt from 'jsonwebtoken';
+
+export function currentUserDataRequest() {
+  return dispatch => {
+    return axios.get('/api/userdata').then(res => {
+      return res.data;
+    });
+  }
+}
---
+++ server/routes/userData.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+let router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('get request received')
+  let token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: 'Failed to Authenticate' });
+    } else {
+      const identifier = decoded.username;
+      User.query({
+        where: { username: identifier }
+      }).fetch({columns: ['username', 'email', 'blurb', 'tagline', 'imageurl']}).then(user => res.json(user))
+    }
+  })
+});
+module.exports = router;
+
