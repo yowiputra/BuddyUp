5770fee06c8bef8d7b0b0d7c6045ef68d44db2dd
Helen <howareyouhelen@hotmail.com>
2017-08-18 21:14:38 +0000

    modified default value as user's value, recieve server data on compatible users on the client side. change state as slider moves, presents compatible users on the match cards on matchmaker event so that users can see their new sets of compatible users as they change their value on the slider.

--- server/sockets.js
+++ server/sockets.js
@@ -8,28 +8,29 @@ module.exports = (io, knex) => {
   let compatUsers = [];
 
   function queryCompatUsers(username, seriousness){
-    knex('users').where('seriousness','>',seriousness-10)
+    return knex('users').where('seriousness','>',seriousness-10)
       .andWhere('seriousness','<',seriousness+10)
       .andWhere('username','!=', username)
       .then(function(results) {
-
         function sortFunction(record1,record2) {
           var difference1 = Math.abs(seriousness - record1.seriousness);
           var difference2 = Math.abs(seriousness - record2.seriousness);
           return difference1 > difference2;
-      }
+        }
         results.sort(sortFunction);
         compatUsers = results;
-        console.log(compatUsers);
-      })
+        return Promise.resolve(compatUsers);
+      });
   }
 
   function queryUser(username){
-    knex('users').where('username', username)
-      .then(function(user) {
-        queryCompatUsers(user[0].username,user[0].seriousness);
-        // console.log(user[0]);
-      })
+    return knex('users').where('username', username);
+  }
+
+  function updateSeriousnessDb(username, value){
+    return knex('users')
+    .where('username', username)
+    .update('seriousness', value)
   }
 
   io.sockets
@@ -39,8 +40,25 @@ module.exports = (io, knex) => {
     })).on('authenticated', function(socket) {
       const currentUserName = socket.decoded_token.username;
       console.log('hello! ' + currentUserName);
-      queryUser(currentUserName);
-    });
+
+      // Initial Load
+      queryUser(currentUserName).then(user => {
+        socket.emit('getDefaultSeriousness', JSON.stringify(user[0].seriousness));
+        queryCompatUsers(user[0].username, user[0].seriousness).then(users => {
+          socket.emit('updateCompatUsers', JSON.stringify(users));
+        });
+      });
+
+      // Update seriousness after slider change
+      socket.on('updateSeriousness', function(data) {
+        const sliderValue = JSON.parse(data).value;
+        updateSeriousnessDb(currentUserName, sliderValue).then(() => {
+          queryCompatUsers(currentUserName, sliderValue).then(users => {
+            socket.emit('updateCompatUsers', JSON.stringify(users));
+          })
+        })
+      });
+    })
 
   io.listen(IO_PORT, () => {
     console.log("Socket.io listening on port " + IO_PORT);
