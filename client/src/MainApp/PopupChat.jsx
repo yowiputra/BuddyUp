9ae5a6d4e786f75eba7c7882d8c3cd3fae0b1d6b
JV Cortez <juanvictor.cortez@gmail.com>
2017-08-18 18:09:29 -0700

    added chat bar and message send button

--- client/src/MainApp/PopupChat.jsx
+++ client/src/MainApp/PopupChat.jsx
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
+//Used react-popupbox because of ease of integration. However, if given more time, would have switched it to the official reactjs 'react-modal' package that has the same functionality https://reactcommunity.org/react-modal/
 import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
 
 class PopupChat extends Component {
-  openPopupbox() {
+  openPopupbox = () => {
     const content = (
       <div>
-        <p className="quotes">Work like you don't need the money.</p>
-        <p className="quotes">Dance like no one is watching.</p>
-        <p className="quotes">And love like you've never been hurt.</p>
-        <span className="quotes-from">― Mark Twain</span>
+        <span>MessageList</span>
+        <footer>
+          <input 
+            className = "chat-message" 
+            placeholder = "Type a message and hit ENTER"
+          />
+        </footer>
+        <button className="demo-button" onClick={ this.updatePopupbox }>Send</button>
       </div>
     )
-    PopupboxManager.open({ content })
+
+    PopupboxManager.open({ 
+      content,
+      config: {
+        titleBar: {
+          enable: true,
+          text: 'From: Username A'
+        }
+      }
+    })
   }
 
+  updatePopupbox = () => {
+    const content = (
+      <div>
+        <span>MessageList</span>
+        <footer>
+          <input 
+            className = "chat-message" 
+            placeholder = "Type a message and hit ENTER"
+          />
+          <button className="demo-button" onClick={ this.openPopupbox }>Send</button>
+        </footer>
+      </div>
+    )
+
+    PopupboxManager.update({
+      content,
+      config: {
+        titleBar: {
+          text:'From: Username B',
+          closeButton: true
+        }
+      }
+    })
+  }
+
+   //sends username text and color to client-side App.jsx when enter is pressed
+  //  onUserPost(keyPressed) {
+  //   const enterPressed = keyPressed.key;
+  //   if (enterPressed === 'Enter') {
+  //     this.props.onNewPost('postNotification',this.state.user,'',this.state.color);
+  //   }
+  // }
   render() {
     return (
     //Pop up currently triggered by a button click
