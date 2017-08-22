c5311a189da2f222608d5ed2737b0cee94f089b1
John <john.wong.h@gmail.com>
2017-08-22 05:52:58 +0000

    Changed room table to have a name column.

--- server/db/migrations/20170822024306_rooms_table.js
+++ server/db/migrations/20170822024306_rooms_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
         knex.schema.createTable('rooms', function(table) {
             table.increments();
+            table.string('name').unique().notNull();
             table.timestamps();
         })
     ])
